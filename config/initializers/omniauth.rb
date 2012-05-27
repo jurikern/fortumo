3b0a21c92da7eb8997fde0002a9e6ada871a3dc2
Juri Semjonov <juri.semjonov@cashongo.co.uk>
2012-05-27 11:55:17 +0300

    Production fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,10 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, '392829774096506', '5e0a65426ac123cce35c3acde0ba81fd', :scope => 'email,offline_access,read_stream' if Rails.env.production?
+  provider :facebook, '315087038570235', 'aebe59dc53bc843a70615b98707cc49f', :scope => 'email,offline_access,read_stream' if Rails.env.development?  
+end
+
 if Rails.env.development?
   OmniAuth.config.full_host = 'http://localhost:3000'
 elsif Rails.env.production?
   Omniauth.config.full_host = 'http//fortumo.cucombo.com'
-end
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '392829774096506', '5e0a65426ac123cce35c3acde0ba81fd', :scope => 'email,offline_access,read_stream' if Rails.env.production?
-  provider :facebook, '315087038570235', 'aebe59dc53bc843a70615b98707cc49f', :scope => 'email,offline_access,read_stream' if Rails.env.development?  
 end
